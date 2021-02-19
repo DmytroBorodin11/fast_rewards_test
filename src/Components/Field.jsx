da5cca64293e4a5c6a44faac259ae80296c7118c
Dmytro Borodin <dimabor96@gmail.com>
2021-02-19 10:39:38 +0200

    setup validation

--- src/Components/Field.jsx
+++ src/Components/Field.jsx
@@ -2,17 +2,43 @@ import React from 'react';
 import '../css/Form.css';
 import icon1 from '../assets/pass_icon_1.svg';
 import icon2 from '../assets/pass_icon_2.svg';
+import {useState} from 'react';
 
 export default function Field(props) {
+
+    let [ invalidField, setInvalidField ] = useState(false);
+
+    console.log(invalidField);
+
+    const checkIfInvalid = (e) => {
+        if (e.target.value === '') {
+            setInvalidField(true);
+            props.updateIsDisabled(true);
+        }else {
+            setInvalidField(false);
+            props.updateIsDisabled(false);
+        }
+    }
+
+    const onChangeFn = (e) => {
+        setInvalidField(false);
+        props.updateIsDisabled(false);
+        props.onChangeFn(e)
+    }
+
+
+
+
     return (
-        <div className={'field__block' + ' ' + (props.class)}>
-            <input type={props.type} placeholder={props.placeholder}/>
-            {props.type === 'password' ? 
-                <button className='show_pass__btn'>
-                    <img src={props.class ? icon2 : icon1}/>
-                </button> : ''}
+        <div className={'field__block' + ' ' + (invalidField ? 'error' : '')}>
+            <input onChange={(e) => {onChangeFn(e)}} onBlur={(e) => {checkIfInvalid(e)}}
+            value={props.value} type={props.type} placeholder={props.placeholder}/>
+            {props.passType === 'password' ? 
+                <div className='show_pass__btn' onClick={props.changePassType}>
+                    <img src={invalidField ? icon2 : icon1}/>
+                </div> : ''}
             {
-                props.class ? <p className="error__message">{props.error}</p> : ''
+                invalidField ? <p className="error__message">{props.error}</p> : ''
             }
         </div>
     )
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,18 +2,55 @@ import React from 'react';
 import '../css/Form.css';
 import Field from './Field';
 import {ExternalLink} from "react-external-link";
+import {useState} from 'react';
 
 function Form(props) {
+
+    let [ emailValue, setEmailValue ] = useState('');
+    let [ passValue, setPassValue ] = useState('');
+    let [ isDisabled, setIsDisabled ] = useState(false);
+    let [ passType, setPassType ] = useState('password');
+
+    const login = (e) => {
+        e.preventDefault();
+        alert(emailValue + ' ' + passValue)
+    }
+
+    const updateEmailValue = (e) => {
+        setEmailValue(e.target.value);
+    }
+
+    const updatePassValue = (e) => {
+        setPassValue(e.target.value);
+    }
+    const updateIsDisabled = (invalidField) => {
+        setIsDisabled(invalidField);
+    }
+
+    const changePassType = () => {
+        if ( passType === 'password') {
+            console.log(passValue);
+            setPassType('text')
+        }else {
+            setPassType('password');
+        }
+     
+    }
+
     return (
-        <form className="login__form">
+        <form className="login__form" onSubmit={(e) => login(e)}>
             <h2 className="form__title">
                 Login
             </h2>
             <div className="fields">
-                <Field placeholder='Email address' type='text'/>
-                <Field placeholder='Password' type='password'/> 
+                <Field onChangeFn={updateEmailValue} error='Email required' updateIsDisabled={updateIsDisabled}
+                placeholder='Email address' type='text'/>
+                <Field onChangeFn={updatePassValue} error='Password required' 
+                changePassType={changePassType} passType='password'
+                updateIsDisabled={updateIsDisabled}
+                placeholder='Password' type={passType}/> 
             </div>
-            <button className="submit__btn">
+            <button disabled={isDisabled} className="submit__btn">
                 Log in
             </button> 
             <div className="form__text">
